beb97a6941107092e00e3c2cf3c1bff493085a10
Dennis Campos <dennisjr89@gmail.com>
2021-03-19 14:38:13 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const API_KEY = "ec89c9f730c73e1aa9bb7e293afc6f93";
+const apiURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+const IMG = "https://image.tmdb.org/t/p/w1280";
+
+async function getMovies() {
+  const res = await fetch(apiURL);
+  const data = await res.json();
+
+  console.log(data);
+  // Movie Information
+
+  // grabs the class .movie box from the html file.
+  const myDiv = document.querySelector(".movie-box");
+  for (let i = 0; i < data.results.length; i++) {
+    const movieTitle = data.results[i].original_title;
+    const movieVotes = data.results[i].vote_average;
+    myDiv.innerHTML += `<img class="movie-pic" src="" alt="movie of picture">
+                    <div class="movie-info">
+                        <h3 class="movie-title">${movieTitle}</h3>
+                        <span class="movie-rating">${movieVotes}</span>
+                        </div>`;
+
+    console.log(myDiv);
+  }
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getMovies();
+
+//next task figure out image.
